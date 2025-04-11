2bfd448ea7f739d9efda021983231c85f12a50d1
ZedBradley <zedbradley30@gmail.com>
2025-04-11 17:38:04 +0000

    update\

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ print(r'( \/\/ )( ___)(  )  / __)(  _  )(  \/  )( ___)    (_  _)(  _  )     / __
 print(r' )    (  )__)  )(__( (__  )(_)(  )    (  )__)       )(   )(_)(     ( (_-. )(__)(  )__) \__ \\__ \     )    (  ) _ (  )(_)(     \__ \ /(__)\  _)(_  )(_) )     _)(_   )(  ')
 print(r'(__/\__)(____)(____)\___)(_____)(_/\/\_)(____)     (__) (_____)     \___/(______)(____)(___/(___/    (__/\__)(_) (_)(_____)    (___/(__)(__)(____)(____/     (____) (__) ')
 
-e_history = ['I came, I saw, I conquered',
-            'Those who cannot remember the past are condemned to repeat it']
+e_history = ["I came, I saw, I conquered",
+            "Those who cannot remember the past are condemned to repeat it"]
 
 eha = ['Julius Caesar',
     'George Santayana ']
@@ -22,9 +22,11 @@ h_history = ['Our greatest glory is not in never falling, but in rising every ti
 hha = ['Confucius',
     'Christopher Columbus']
 
-e_fict = ['I am your father']
+e_fiction = ["I am your father",
+        "Winter is coming"]
 
-efa = ['Darth Vader']
+efa = ['Darth Vader',
+        'Every Stark ever']
 
 
 m_fict = ['a']
@@ -33,19 +35,32 @@ m_fict = ['a']
 h_fict = ['a']
 
 
-number_generator(1,10)
+def number_generator(a,b):
+    return random.randint(a,b)
 
-print = 'You will be guessing if the quote is from history or from fiction! Are you Ready?'
+print = 'You will be guessing if the quote is from history or from fiction! Are you ready?'
 
 difficulty = input('easy, medium, hard, or exit: ')
-while true
-    if difficulty = 'easy':
-        
-    if difficulty = 'medium':
-        
-    if difficulty = 'hard':
+while True:
+    if difficulty == 'easy':
+        num = number_generator(0,1)
+        if number_generator(1,10) % 2 == 0:
+            print(e_history[num])
+            answer = input('Enter your answer (history or fiction): ')
+            if answer == 'history' or answer == 'History':
+                print(f'You did it! That one was by {eha[num]}')
+        else:
+            print(e_fiction[num])
+            answer = input('Enter your answer (history or fiction): ')
+            if answer == 'fiction' or answer == 'Fiction':
+                print(f'You did it! That one was by {efa[num]}')
 
-    if difficulty = 'exit':
+        
+    # if difficulty == 'medium':
+        
+    # if difficulty == 'hard':
+
+    if difficulty == 'exit':
         print('Thanks for playing')
         break
         
