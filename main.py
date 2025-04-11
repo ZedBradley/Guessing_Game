f5535d5cd0b16e9d0013235ebd6f3f7f26040536
ZedBradley <zedbradley30@gmail.com>
2025-04-11 17:08:00 +0000

    beginning

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
+import random
+print(r' _    _  ____  __    ___  _____  __  __  ____      ____  _____       ___  __  __  ____  ___  ___      _    _  _   _  _____      ___    __    ____  ____       ____  ____ ')
+print(r'( \/\/ )( ___)(  )  / __)(  _  )(  \/  )( ___)    (_  _)(  _  )     / __)(  )(  )( ___)/ __)/ __)    ( \/\/ )( )_( )(  _  )    / __)  /__\  (_  _)(  _ \     (_  _)(_  _)')
+print(r' )    (  )__)  )(__( (__  )(_)(  )    (  )__)       )(   )(_)(     ( (_-. )(__)(  )__) \__ \\__ \     )    (  ) _ (  )(_)(     \__ \ /(__)\  _)(_  )(_) )     _)(_   )(  ')
+print(r'(__/\__)(____)(____)\___)(_____)(_/\/\_)(____)     (__) (_____)     \___/(______)(____)(___/(___/    (__/\__)(_) (_)(_____)    (___/(__)(__)(____)(____/     (____) (__) ')
 
+e_history = ['I came, I saw, I conquered',
+            'Those who cannot remember the past are condemned to repeat it']
+
+eha = ['Julius Caesar',
+    'George Santayana ']
+
+
+m_history = ['History is a set of lies agreed upon']
+
+mha = ['Napoleon Bonaparte']
+
+
+h_history = ['Our greatest glory is not in never falling, but in rising every time we fall',
+            'Following the light of the sun, we left the Old World']
+
+hha = ['Confucius',
+    'Christopher Columbus']
+
+e_fict = ['I am your father']
+
+efa = ['Darth Vader']
+
+
+m_fict = ['a']
+
+
+h_fict = ['a']
+
+
+number_generator(1,10)
+
+print = 'You will be guessing if the quote is from history or from fiction! Are you Ready?'
+
+difficulty = input('easy, medium, hard, or exit: ')
+while true
+    if difficulty = 'easy':
+        
+    if difficulty = 'medium':
+        
+    if difficulty = 'hard':
+
+    if difficulty = 'exit':
+        print('Thanks for playing')
+        break
+        
+    else:
+        print("check your spelling")                                                                                                                                                                                                  
